e8b7162a0a5913466d02d11c422ac70916ddcb7f
Davis Haupt <dbh937@gmail.com>
2012-12-13 20:40:24 -0500

    Accelerometer test program

--- Accelerometer_Test/Accelerometer_Test.ino
+++ Accelerometer_Test/Accelerometer_Test.ino
@@ -1,1 +1,11 @@
+void setup() {
+  Serial.begin(9600);
+}
 
+void loop() {
+  Serial.print("x: "); Serial.println(analogRead(0));
+  Serial.print("y: "); Serial.println(analogRead(1));
+  Serial.print("z: "); Serial.println(analogRead(2));
+  Serial.println(' ');
+  delay(500);
+}
