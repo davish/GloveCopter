38a1d517c2ea0134369b8f53e7de5c0a88166202
Davis Haupt <dbh937@gmail.com>
2012-12-18 18:09:44 -0500

    Adjusted trim

--- GloveCopter.ino
+++ GloveCopter.ino
@@ -2,7 +2,7 @@
 const int LED = 3; // The LED
 const int FREQ = 38000; // The carrier frequency
 const int ROTATION_STATIONARY = 63;
-const int CAL = -7; // Trim
+const int CAL = -5; // Trim
 // const int TRIMPOT = 0;
 
 long previousMicros = 0;
