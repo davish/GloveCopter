b56fb7f9a407d805509ace6e977222c25467732c
Davis Haupt <dbh937@gmail.com>
2013-01-02 16:00:21 -0500

    Switched pins to new layout

--- Accelerometer_Test/Accelerometer_Test.ino
+++ Accelerometer_Test/Accelerometer_Test.ino
@@ -3,9 +3,9 @@ void setup() {
 }
 
 void loop() {
-  Serial.print("x: "); Serial.println(analogRead(0));
-  Serial.print("y: "); Serial.println(analogRead(1));
-  Serial.print("z: "); Serial.println(analogRead(2));
+  Serial.print("x: "); Serial.println(analogRead(1));
+  Serial.print("y: "); Serial.println(analogRead(0));
+//  Serial.print("z: "); Serial.println(analogRead(2));
   Serial.println(' ');
   delay(500);
 }
